c7a44205c197e70a1a444e292f94565124e9daa4
Jeff Collins <tara.sl.collins@gmail.com>
2023-08-01 11:35:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,91 @@
-let vowels = document.querySelectorAll(".vowel");
-var inventory = []
+const vowels = document.querySelectorAll(".vowel");
+const consonants = document.querySelectorAll(".consonant");
+const labels = document.querySelectorAll(".label");
+const dialectSelect = document.getElementById('dialect_selector');
+let inventory = []
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d")
+const GAE = ["ncnfu", "cfu", "ncnbr", "cbr", "omfu", "cmfu", "mcv", "mbr", "nofv", "nocv", "ocu", "obr"]
+const dialects = {
+  "empty": [],
+  "GAE": GAE
+}
 
 ctx.beginPath()
 ctx.moveTo(25, 35)
-ctx.lineTo(60,100)
-ctx.moveTo(70,125)
-ctx.lineTo(105,190)
-ctx.moveTo(115,215)
-ctx.lineTo(145,280)
-ctx.moveTo(45,20)
-ctx.lineTo(150,20)
-ctx.moveTo(200,20)
-ctx.lineTo(305,20)
+ctx.lineTo(60, 100)
+ctx.moveTo(70, 125)
+ctx.lineTo(105, 190)
+ctx.moveTo(115, 215)
+ctx.lineTo(145, 280)
+ctx.moveTo(45, 20)
+ctx.lineTo(150, 20)
+ctx.moveTo(200, 20)
+ctx.lineTo(305, 20)
+ctx.moveTo(330, 35)
+ctx.lineTo(330, 100)
+ctx.moveTo(330, 125)
+ctx.lineTo(330, 190)
+ctx.moveTo(330, 215)
+ctx.lineTo(330, 280)
+ctx.moveTo(305, 290)
+ctx.lineTo(265, 290)
+ctx.moveTo(215, 290)
+ctx.lineTo(170, 290)
+ctx.moveTo(90, 110)
+ctx.lineTo(170, 110)
+ctx.moveTo(220, 110)
+ctx.lineTo(305, 110)
+ctx.moveTo(135, 200)
+ctx.lineTo(190, 200)
+ctx.moveTo(240, 200)
+ctx.lineTo(305, 200)
+ctx.moveTo(175, 35)
+ctx.lineTo(192, 100)
+ctx.moveTo(197, 125)
+ctx.lineTo(212, 190)
+ctx.moveTo(217, 215)
+ctx.lineTo(235, 280)
 ctx.stroke()
 
+let setPhoneme = function(phoneme, value) {
+  phon = document.querySelector("#".concat(phoneme))
+  var idx = inventory.indexOf(phoneme)
+  if (value == 0) {
+    phon.classList.remove("selected");
+    if (idx != -1) {
+      inventory.splice(idx, 1)
+    }
+
+  } else if (value == 1) {
+    phon.classList.add("selected");
+    if (idx == -1) {
+      inventory.push(phoneme)
+    }
+
+  } else {
+    if (idx == -1) {
+      inventory.push(phoneme)
+    } else {
+      inventory.splice(idx, 1)
+    }
+    phon.classList.toggle("selected");
+
+  }
+}
+
+dialectSelect.addEventListener('change', function handleChange(event) {
+  dialect = event.target.value;
+  if (dialect == "null") {
+    return
+  }
+  vowels.forEach(function(currentValue, currentIndex, listObj) {
+    setPhoneme(currentValue.id, dialects[dialect].includes(currentValue.id) ? 1 : 0);
+  });
+  consonants.forEach(function(currentValue, currentIndex, listObj) {
+    setPhoneme(currentValue.id, dialects[dialect].includes(currentValue.id) ? 1 : 0);
+  });
+})
 
 vowels.forEach(function(currentValue, currentIndex, listObj) {
   currentValue.addEventListener("mousedown", function(e) {
@@ -24,13 +94,32 @@ vowels.forEach(function(currentValue, currentIndex, listObj) {
       audio.play();
     }
     if (e.button == 0) {
-      var idx = inventory.indexOf(currentValue.id);
-      if (idx == -1) {
-        inventory.push(currentValue.id)
-      } else {
-        inventory.splice(idx, 1)
-      }
-      currentValue.classList.toggle("selected");
+      setPhoneme(currentValue.id, -1);
+      dialectSelect.value = "null"
+    }
+  }, false);
+});
+
+consonants.forEach(function(currentValue, currentIndex, listObj) {
+  currentValue.addEventListener("mousedown", function(e) {
+    if (e.button == 2) {
+      var audio = document.getElementById(currentValue.id.concat("_", "audio"));
+      audio.play();
+    }
+    if (e.button == 0) {
+      setPhoneme(currentValue.id, -1);
+      dialectSelect.value = "null"
+    }
+  }, false);
+});
+
+labels.forEach(function(currentValue, currentIndex, listObj) {
+  currentValue.addEventListener("mousedown", function(e) {
+    if (e.button == 0) {
+      inner = currentValue.offsetParent.querySelector(".inner_container")
+      if (inner.style.display == "none") {
+      inner.style.display = "flex";}
+      else {inner.style.display = "none"}
     }
   }, false);
 });
